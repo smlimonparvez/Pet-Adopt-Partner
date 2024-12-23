cde17f3f0388deb34b26a0a4cc293fadb66d229a
smlimonparvez <smlimonparvez@gmail.com>
2024-12-23 23:00:57 +0600

    Image Container Created

--- js/main.js
+++ js/main.js
@@ -37,6 +37,17 @@ function startCountdown() {
   }, 1000);
 }
 
+// image container
+const imageContainer = (image, pet_name) => {
+  console.log( pet_name);
+  const imageContainer = document.getElementById('image-container');
+  const imageDiv = document.createElement('div'); 
+  imageDiv.innerHTML = ` 
+    <img src="${image}" alt="${pet_name}" class="w-28 h-28  object-cover rounded-lg flex-grow ">
+  `;
+  imageContainer.appendChild(imageDiv);
+}
+
 // dynamic card
 const loadCard = () => {
     fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
@@ -54,6 +65,8 @@ const loadCardByCategory = (category) => {
 const displayCard = (pets) => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
+
+    // No Pets Available
     if(pets.length === 0){
       cardContainer.classList.remove('grid');
       cardContainer.innerHTML = `
@@ -66,10 +79,11 @@ const displayCard = (pets) => {
     else{
       cardContainer.classList.add('grid');
     }
+
     pets.forEach(element => {
         // console.log(element.pet_name);
         const { petId, breed, category, date_of_birth, pet_details, image, price, gender, vaccinated_status, pet_name } = element;
-
+        
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
        <div class="p-4 bg-base-100 rounded-lg shadow-lg border">
@@ -110,7 +124,7 @@ const displayCard = (pets) => {
             <hr class="my-3 w-10/12 mx-auto border-blue-400">
 
             <div class="flex justify-around">
-             <button onclick="btnLikeShow()" id="btn-like" class="btn bg-white border-blue-200 rounded-lg px-4 hover:border-blue-400"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+             <button onclick="imageContainer('${image}', '${pet_name}')" id="btn-like" class="btn bg-white border-blue-200 rounded-lg px-4 hover:border-blue-400"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
                 </svg>
              </button>
